1a721aa8cd2f9d4e7420112aa9c1bb1575f83f76
gg-harper <osstia@list.ru>
2023-09-01 17:50:11 +0300

    added add and delete methods

--- Tools.pm
+++ Tools.pm
@@ -1,7 +1,7 @@
 package Tools;
 
 use Exporter qw(import);
-@EXPORT = qw(v_login read_user_list);
+@EXPORT = qw(v_login read_user_list add_user delete_user);
 
 sub v_login {
     (my $name, my $password) = @_;
@@ -15,8 +15,48 @@ sub v_login {
     return 0;
 }
 
+sub add_user {
+    (my $name, my $password) = @_;    
+    # open (my $hWRITE_FILE, '>', "users") or die("File not found"); 
+
+    if(v_login($name, $password)) {
+        print("User already exists!\n");
+        return 0;
+    }
+    else {
+       open (my $hWRITE_FILE, '>>', "users") or die("File not found"); 
+       print ($hWRITE_FILE "\n$name=$password");
+       close($hWRITE_FILE);
+       return 1;
+    }
+    close($hWRITE_FILE);
+}
+
+sub delete_user {
+    my $user_name = $_[0];
+    my $is_deleted = 0;
+    open (my $hWRITE_FILE, '>', "_users") or die("File not found");
+    open (my $hREAD_FILE, '<', "users") or die("File not found");
+    if(!v_login) {
+        re
+    }
+    while(my $user = <$hREAD_FILE>) {
+        if($user =~ /$user_name/) {
+            $is_deleted = 1;
+            next;
+        }
+         print($hWRITE_FILE $user);
+    }
+    close($hWRITE_FILE);
+    close($hREAD_FILE);
+    
+    rename("_users", "users");
+    return $is_deleted;
+}
+
 sub read_user_list {
-    open (my $hREAD_FILE, '<', "users");
+    open (my $hREAD_FILE, '<', "users") or die("File not found");
+    
     my %users = ();
     while(my $input_line = <$hREAD_FILE>) {
         $input_line = parse_line($input_line);
--- les2.pl
+++ les2.pl
@@ -6,13 +6,38 @@ use lib ".";
 
 use Tools;
 
+sub main {
+    while(1) {
+            if (@ARGV == 3 && $ARGV[2] eq 'login') {
+                (my $name, my $password) = @ARGV;
+                if (v_login($name, $password)) {
+                    print("Welcome, $name!\n");
+                }
+                else {
+                    print("Wrong user or password!\n");
+                }
+                last;
+            }
 
-if (@ARGV == 3 && $ARGV[2] eq 'login') {
-     (my $name, my $password) = @ARGV;
-    if (v_login($name, $password)) {
-        print("Welcome, $name!\n");
+            if (@ARGV == 3 && $ARGV[2] eq 'add') {
+                (my $name, my $password) = @ARGV;
+                if (add_user($name, $password)) {
+                    print("User added successfully!\n");
+                }
+                last;
+            }
+
+              if (@ARGV == 2 && $ARGV[1] eq 'delete') {
+                my $name = shift(@ARGV);
+                if (delete_user($name)) {
+                    print("User removed successfully!\n");
+                }
+                else {
+                    print("No user $name.\n");
+                }
+                last;
+            }
     }
-    else {
-        print("Wrong user or password!\n");
-    }
+    
 }
+main();
