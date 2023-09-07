9b80f86d234b14deabbbedca98c75336e92f722a
gg-harper <osstia@list.ru>
2023-09-07 15:56:58 +0300

    fix integer answer

--- Penkov.pl
+++ Penkov.pl
@@ -3,6 +3,7 @@
 #
 use strict;
 use warnings;
+use POSIX qw/ceil/;
 
 print("in: \n");
 my $input = <STDIN>;
@@ -12,7 +13,7 @@ print("backlog: \n");
 my $backlog = <STDIN>;
 
 my $result = _calculate($input, $output, $backlog);
-my $answer_string = $result > 0 ? "Успеет за $result" : 'Не успеет';
+my $answer_string = $result > 0 ? "Успеет за $result спринт(ов)" : 'Не успеет';
 
 print("$answer_string");
 print("\n");
@@ -21,6 +22,6 @@ print("\n");
 sub _calculate {
 	my ($in, $out, $backlog) = @_;
 		
-	return $backlog / (($out  - $in) * 10); 
+	return ceil($backlog / (($out  - $in) * 10)); 
 }
 
